0cd1280ace64dd1109d1fc5eb1c3c7cb78965971
Diego Rojas <darb302@gmail.com>
2024-06-27 23:05:15 +0000

    set incremental model for sales

---
+++ models/example/sales.sql
@@ -0,0 +1,22 @@
+{{
+  config(
+    materialized = 'incremental',
+    full_refresh = False
+  )
+}}
+
+with sales as (
+    
+    select 
+        *,
+        current_timestamp as current_timestamp_at
+    from {{ source('databricks_source', 'int_sales') }}
+)
+
+select *
+from sales
+{% if is_incremental() %}
+
+where current_timestamp_at > (select max(current_timestamp_at) from {{ this }})
+
+{% endif %}
